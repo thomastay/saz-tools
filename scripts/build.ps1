9531837f37615fadd3130a4eab28165de883d65d
Thomas Tay <Tay.Thomas@microsoft.com>
2022-12-12 22:09:42 -0800

    build

---
+++ scripts/build.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = "Stop"
+$packages = ("sazdump", "sazserve")
+$platforms = ("windows", "darwin")
+$arches = "amd64"
+$env:GO111Modules = 1
+
+foreach ($platform in $platforms) {
+	$env:GOOS = $platform
+	foreach ($arch in $arches) {
+		$env:GOARCH = $arch
+		foreach ($package in $packages) {
+			$name = "$package-$platform-$arch"
+			if ($platform -eq "windows") {
+				$name = "$package-$platform-$arch.exe"
+			}
+			$name = "$package-$platform-$arch"
+			go build -ldflags="-s -w" -o bin/$name "./cmd/$package"
+		}
+	}
+}
+
